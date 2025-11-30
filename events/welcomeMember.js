dbb3e342c9802c5536c6ac524be20cc3a850e9ec
DmTriet31 <dmtriet.31@gmail.com>
2025-11-30 15:47:41 +0100

    Update welcomeMember.js

--- events/welcomeMember.js
+++ events/welcomeMember.js
@@ -21,7 +21,7 @@ module.exports = (client) => {
 
     const embed = new EmbedBuilder()
       .setColor(0xff4757)
-      .setTitle('<a:RL_rainbowchloe:1376245313853063250> Welcome To Mango <a:RL_rainbowchloe:1376245313853063250>')
+      .setTitle('<a:pink_bow:1444701287818989764> Welcome To Mango <a:pink_bow:1444701287818989764>')
       .setDescription(
         `Chúc bạn có những khoảng khắc vui vẻ và gắn kết với mọi người khi tham gia server, đừng ngần ngại trò chuyện và kết bạn với các thành viên khác nhé.\n\n` +
         `Hãy thoải mái tham gia các cuộc trò chuyện, đóng góp ý tưởng và cùng nhau xây dựng 1 cộng đồng vui vẻ và đoàn kết. ` +
